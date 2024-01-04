75680b10dc67d73df141b4a4f202d0fb0a86a767
Katakishi Zzzzzzzz <80763333+katakishi0@users.noreply.github.com>
2024-01-03 22:02:05 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,16 +2,16 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
+export BRANCH="twrp-11" # the branch of manifest
 
 # about your device
-export DEVICE="j6lte" # codename used in device tree
-export DT_LINK="https://github.com/samsungexynos7870/android_device_samsung_j6lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="channel" # codename used in device tree
+export DT_LINK="https://github.com/SyberHexen/android_device_motorola_channel/tree/android-11" # device tree link
+export DT_BRANCH="android-11" # device tree branch
+export VENDOR="motorola" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
